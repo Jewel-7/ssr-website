5c659998790d5fb45f42524d1a0c841495d973f1
jewel-7 <jewel.simon@raweng.com>
2021-01-13 19:59:07 +0530

    IMplemnetd styling and made responsive

--- components/Slides.jsx
+++ components/Slides.jsx
@@ -4,11 +4,6 @@ import { Slide } from "react-slideshow-image";
 import styles from "../styles/Home.module.css";
 import "react-slideshow-image/dist/styles.css";
 
-const slideImages = [
-  "https://www.raweng.com/v3/assets/bltaacb6b0c9b693c2d/blt78bd9fc2084185e1/5e31675d7ca70442647a5061/Who-we-Are.png?format=pjpg&width=1210",
-  "https://media-exp1.licdn.com/dms/image/C4D1BAQGORgdnW9tgiQ/company-background_10000/0/1591965978994?e=2159024400&v=beta&t=muB5jWIpPh0BlE8Qn64yp7s-jcGSD8-W0VAMs5VGzWE",
-  "https://digitalmarketingsupermarket.com/wp-content/uploads/2020/02/www_contentstack_com_1024_768.png",
-];
 const properties = {
   duration: 2000,
   transitionDuration: 800,
@@ -22,13 +17,26 @@ const Slideshow = () => {
     <div className={styles["slide"]}>
       <Slide {...properties}>
         <div className={styles["each-slide1"]}>
-          <div style={{ backgroundImage: `url(${slideImages[0]})` }}></div>
+          <img
+            className={styles["slide-image1"]}
+            src="https://www.raweng.com/v3/assets/bltaacb6b0c9b693c2d/blt78bd9fc2084185e1/5e31675d7ca70442647a5061/Who-we-Are.png?format=pjpg&width=1210"
+            alt=""
+          />
+        </div>
+
+        <div className={styles["each-slide3"]}>
+          <img
+            className={styles["slide-image3"]}
+            src="https://digitalmarketingsupermarket.com/wp-content/uploads/2020/02/www_contentstack_com_1024_768.png"
+            alt=""
+          />
         </div>
         <div className={styles["each-slide2"]}>
-          <div style={{ backgroundImage: `url(${slideImages[1]})` }}></div>
-        </div>
-        <div className={styles["each-slide3"]}>
-          <div style={{ backgroundImage: `url(${slideImages[2]})` }}></div>
+          <img
+            className={styles["slide-image2"]}
+            src="https://images.contentstack.io/v3/assets/blt137524604e62b5cb/blt97e1e009fc35ebee/5edf6b23e92db173fb37dd2a/RE-office2@2x.png?auto=webp&quality=100&format=pjpg"
+            alt=""
+          />
         </div>
       </Slide>
     </div>
