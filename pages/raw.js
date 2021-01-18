10cb17e1287d06032bf14e0858088e9a0042beb5
jewel-7 <jewel.simon@raweng.com>
2021-01-18 23:23:07 +0530

    data Spelling

--- pages/raw.js
+++ pages/raw.js
@@ -6,6 +6,8 @@ import CompanyPage from "../components/CompanyPage";
 import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
 
+/ const url = "https://heroku-json-data-server.herokuapp.com/companies";
+
 class Users extends Component {
   render() {
     return (
@@ -15,7 +17,7 @@ class Users extends Component {
         {this.props.users.map((user) => {
           return <CompanyPage key={user.id} user={user} />;
         })}
-        <h1 className={styles["des-head"]}>Our Employees</h1>
+        <h1 className={styles["des-head"]}>Our Employee</h1>
         <p className={styles["description1"]}>
           {" "}
           <Link href="/rawList">
